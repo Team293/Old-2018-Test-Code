3f1bd0c0189a449b72604f62eea0590f6cbb4ad0
Team293 <293.First@gmail.com>
2018-01-26 18:22:11 -0500

    Winches done

--- src/org/usfirst/frc/team293/robot/subsystems/Afterburner.java
+++ src/org/usfirst/frc/team293/robot/subsystems/Afterburner.java
@@ -1,18 +1,22 @@
 package org.usfirst.frc.team293.robot.subsystems;
 
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
-public class Afterburner extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+public class Afterburner extends Subsystem {
+	VictorSP theGodWinch = new VictorSP(10);
+
+	public void winchOn(){
+		theGodWinch.set(1);
+	}
+	
+	public void winchOff(){
+		theGodWinch.set(0);
+	}
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    	winchOff();
     }
 }
 
--- src/org/usfirst/frc/team293/robot/subsystems/Winch.java
+++ src/org/usfirst/frc/team293/robot/subsystems/Winch.java
@@ -1,15 +1,23 @@
 package org.usfirst.frc.team293.robot.subsystems;
-
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.VictorSP;
 
-/**
- *
- */
+
+
+
+
 public class Winch extends Subsystem {
+	VictorSP theActualWinch = new VictorSP(1);
+	
+	public void winchOn(){
+		theActualWinch.set(1);
+	}
+	
+	public void winchOff(){
+		theActualWinch.set(0);
+	}
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
