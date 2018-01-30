7c6e53c21af8ff3ac422df01d3e98a7d0f896e61
Bongo-Drum <30634789+Bongo-Drum@users.noreply.github.com>
2018-01-30 14:57:54 -0500

    Update Afterburner.java

--- src/org/usfirst/frc/team293/robot/subsystems/Afterburner.java
+++ src/org/usfirst/frc/team293/robot/subsystems/Afterburner.java
@@ -6,25 +6,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Afterburner extends Subsystem {
 	VictorSP theGodWinch = new VictorSP(10);
-	/* VictorSP number2 = new VictorSP(#);
-	   VictorSP number3 = new VictorSP(#);
-	   VictorSP number4 = new VictorSP(#);
-        */
 
 	public void winchOn(){
 		theGodWinch.set(1);
-	      /*number2.set(1);
-	        number3.set(1);
-		number4.set(1);
-	      */
 	}
 	
 	public void winchOff(){
 		theGodWinch.set(0);
-	      /*number2.set(0);
-	        number3.set(0);
-		number4.set(0);
-	      */
 	}
 
     public void initDefaultCommand() {
